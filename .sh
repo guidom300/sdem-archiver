9da3ed8c3254beb0e34c960250166ea803e2edfd
Alex Tavernari <a.tavernari@gmail.com>
2014-11-19 18:31:35 +0100

    Simplify creation of xcode projects

---
+++ .sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+alias find_project_files="find -s . -type f -name '*.pro'"
+alias make_xcodeproj="qmake -spec macx-xcode"
+
+generate_xcode_projects() {
+    for pro in $(find_project_files -mindepth 2); do
+        (cd "$(dirname "$pro")" && make_xcodeproj "$(basename "$pro")")
+    done
+
+    make_xcodeproj "$(find_project_files -depth 1)"
+}
+
+alias find_xcodeproj="find . -type d -name '*.xcodeproj'"
+
+rm_xcode_projects() {
+    rm -rv $(find_xcodeproj)
+}
