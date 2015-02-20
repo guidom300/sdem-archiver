d60470460d740e355a68695449b1aef4354ee210
Alex Tavernari <a.tavernari@gmail.com>
2015-02-20 17:38:47 +0100

    Use parallel.h

--- app/success.cpp
+++ app/success.cpp
@@ -5,12 +5,8 @@
 #include <fstream>
 #include <ios>
 #include <iterator>
-#include "splitter.h"
-#include "worker.h"
-#include "encoder_wrapper.h"
-#include "lzss_encoder.h"
-#include "huffman_encoder_stack.h"
 #include <QDebug>
+#include "parallel.h"
 
 using namespace std;
 
@@ -26,8 +22,8 @@ Success::Success(QString input, QString output, int nThreads, bool dictType, QWi
     ui->pushButton->setDisabled(true);
     ui->labelSuccess->setText("");
     ui->lcd->display(0);
-    running = false;
-    startEncoder = new std::thread(start, (const char*) _input.toLocal8Bit().data(), (const char*) _output.toLocal8Bit().data(), _nThreads, _dictType, &running);
+    running = true;
+    startEncoder = new std::thread(start, _input.toStdString().c_str(), _output.toStdString().c_str(), _nThreads, _dictType, &running);
     clockThread = new std::thread(timeUpgrade, &currentTime, ui, &running, _input, _output);
 }
 
@@ -66,23 +62,7 @@ void Success::timeUpgrade(QTime* currentTime, Ui::Success *ui, bool* running, QS
 
 void Success::start(const char* input, const char* output, int nThreads, bool dictType, bool* running)
 {
-    *running = true;
+    encode_in_parallel(input, output, nThreads);
 
-    ifstream input_file(input, ios::binary);
-      ofstream output_file(output, ios::binary | ios::trunc);
-
-      input_file >> noskipws;
-
-      Splitter<Worker<EncoderWrapper<LZSSEncoder<12, 4>>>,
-               HuffmanEncoderStack<char>> splitter;
-
-      splitter(std::istreambuf_iterator<char>(input_file.rdbuf()),
-               std::istreambuf_iterator<char>(),
-               std::ostreambuf_iterator<char>(output_file),
-               nThreads,
-               32 * 1024);
-
-      *running = false;
-
+    *running = false;
 }
-
